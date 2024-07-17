6d90a01b2b80a9a59b0eb03814dcefbeb1bf6f0c
somtola <somtola@gmail.com>
2024-07-17 16:20:01 +0700

    new commit

--- bot/bot.js
+++ bot/bot.js
@@ -1,5 +1,7 @@
-const { Telegraf } = require("telegraf");
+import { Telegraf } from "telegraf";
 const TOKEN = "7414027722:AAG8xdMf_wIrQFoFVh1588AmvRlHeB8dUYA";
 const bot = new Telegraf(TOKEN);
-bot.start((ctx) => ctx.reply("Welcome"));
+bot.start((ctx) =>
+  ctx.reply("Welcome to food-app")
+);
 bot.launch();
